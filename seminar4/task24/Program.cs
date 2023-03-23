dc5637c9c7e13c2cb628f45a7544bf5a20b76efa
Lora <adresat_743@mail.ru>
2023-03-23 19:23:46 +0300

    task24

---
+++ seminar4/task24/Program.cs
@@ -0,0 +1,26 @@
+﻿//напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+//7-> 28
+//4-> 10
+//8-> 36
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumToA(int a)
+{
+    int sum = 0;
+    for(int i =1; i <= a; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int number = ReadNumber("Введите число А");
+int result = SumToA(number);
+Console.WriteLine(result);
