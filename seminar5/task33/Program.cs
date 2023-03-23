b3df0ec5d35da317c02084a34aeac45018931ff0
Lora <adresat_743@mail.ru>
2023-03-23 19:09:00 +0300

    task33

---
+++ seminar5/task33/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу замена элементов массива: полож. элементы замените на соответствующие отриц., и наоборот.
+//[-4.-8.8.2] -> [4.8.-8.-2]
+
+int[] GetRandomArray(int Length, int leftRange, int righRange)
+{
+    int[] array = new int[Length];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, righRange + 1);
+    }
+
+    return array;
+}
+
+void InvertElements(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int Length0fArray = 12;
+int leftBorder = -9;
+int righBorder = 9;
+
+int[] myArray = GetRandomArray(Length0fArray, leftBorder, righBorder);
+
+Console.WriteLine($"[{string.Join(",", myArray)}]");
+InvertElements(myArray);
+
+Console.WriteLine($"[{string.Join(",", myArray)}]");
