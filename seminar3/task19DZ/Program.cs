a20842b898fd65b4b6dfe810dcb64c889024e5ae
Lora <adresat_743@mail.ru>
2023-03-23 19:26:19 +0300

    task19DZ

---
+++ seminar3/task19DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int ReadNumber(string message);
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int N = ReadNumber("Введите число");
+for(int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i * i + ", ");
+}
+Console.WriteLine(N * N + ".");
+
